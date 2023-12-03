beb247bfa89c7fcaad5d7f12199dead052bdf244
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-12-03 20:00:20 +0200

    Loader for better ux has been added

---
+++ app/photos/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loader from '../ui/Loader';
+
+const Loading = () => {
+  return (
+    <>
+      <Loader />
+    </>
+  );
+};
+
+export default Loading;
---
+++ app/ui/Loader.tsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <div className='container'>
+        <div className='loader__content'>
+          <span className='loader__item'></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
